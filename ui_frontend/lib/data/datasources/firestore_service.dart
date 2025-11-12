e4d0d2cf325692aa3dc2778f51839ab811a7face
Muen1 <c.mutie@gmail.com>
2025-11-12 17:10:43 +0200

    implement FirestoreService with CRUD for users, transactions, subscriptions

---
+++ ui_frontend/lib/data/datasources/firestore_service.dart
@@ -0,0 +1,94 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/user_model.dart';
+import '../models/expense_model.dart';
+import '../models/subscription_model.dart';
+
+class FirestoreService {
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  // -------------------
+  // USERS
+  // -------------------
+  Future<void> createUser(UserModel user) async {
+    await _db.collection('users').doc(user.uid).set(user.toJson());
+  }
+
+  Future<UserModel?> getUser(String uid) async {
+    final doc = await _db.collection('users').doc(uid).get();
+    if (doc.exists && doc.data() != null) {
+      return UserModel.fromJson(doc.data()!);
+    }
+    return null;
+  }
+
+  Future<void> updateUser(UserModel user) async {
+    await _db.collection('users').doc(user.uid).update(user.toJson());
+  }
+
+  Future<void> deleteUser(String uid) async {
+    await _db.collection('users').doc(uid).delete();
+  }
+
+  // -------------------
+  // EXPENSES / TRANSACTIONS
+  // -------------------
+  Future<void> addExpense(String uid, ExpenseModel expense) async {
+    await _db
+        .collection('users')
+        .doc(uid)
+        .collection('transactions')
+        .doc(expense.id)
+        .set(expense.toJson());
+  }
+
+  Future<List<ExpenseModel>> getExpenses(String uid) async {
+    final snapshot =
+        await _db.collection('users').doc(uid).collection('transactions').get();
+
+    return snapshot.docs
+        .where((doc) => doc.data() != null)
+        .map((doc) => ExpenseModel.fromJson(doc.data()))
+        .toList();
+  }
+
+  Future<void> updateExpense(String uid, ExpenseModel expense) async {
+    await _db
+        .collection('users')
+        .doc(uid)
+        .collection('transactions')
+        .doc(expense.id)
+        .update(expense.toJson());
+  }
+
+  Future<void> deleteExpense(String uid, String expenseId) async {
+    await _db
+        .collection('users')
+        .doc(uid)
+        .collection('transactions')
+        .doc(expenseId)
+        .delete();
+  }
+
+  // -------------------
+  // SUBSCRIPTIONS
+  // -------------------
+  Future<void> addSubscription(String uid, SubscriptionModel sub) async {
+    await _db.collection('subscriptions').doc(uid).set(sub.toJson());
+  }
+
+  Future<SubscriptionModel?> getSubscription(String uid) async {
+    final doc = await _db.collection('subscriptions').doc(uid).get();
+    if (doc.exists && doc.data() != null) {
+      return SubscriptionModel.fromJson(doc.data()!);
+    }
+    return null;
+  }
+
+  Future<void> updateSubscription(String uid, SubscriptionModel sub) async {
+    await _db.collection('subscriptions').doc(uid).update(sub.toJson());
+  }
+
+  Future<void> deleteSubscription(String uid) async {
+    await _db.collection('subscriptions').doc(uid).delete();
+  }
+}
